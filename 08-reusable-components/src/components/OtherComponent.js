e697b182606355fabfa3cbab37675f1c8b5e1994
Viacheslav Sukhenko <Viacheslav.Sukhenko@syngenta.com>
2024-07-18 12:39:22 +0300

    add project with reusable components

---
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,16 @@
+function OtherComponent() {
+  return (
+    <div>
+      <p>
+        Sylvester Gardenzio Stallone (/stəˈloʊn/; born July 6, 1946) is an
+        American actor and filmmaker. He has received numerous accolades,
+        including a Golden Globe Award and a Critics' Choice Award, as well as
+        nominations for three Academy Awards and two BAFTA Awards. Stallone is
+        one of only two actors in history (alongside Harrison Ford) to have
+        starred in a box-office No. 1 film across six consecutive decades.
+      </p>
+    </div>
+  );
+}
+
+export default OtherComponent;
