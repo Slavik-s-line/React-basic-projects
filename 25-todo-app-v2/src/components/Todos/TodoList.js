a548a8cad8df22aa34b872e246951e2c1430bc43
Viacheslav Sukhenko <Viacheslav.Sukhenko@syngenta.com>
2024-08-18 22:58:39 +0300

    add todo-app-v2

---
+++ 25-todo-app-v2/src/components/Todos/TodoList.js
@@ -0,0 +1,22 @@
+import Todo from './Todo';
+import styles from './TodoList.module.css';
+
+const TodoList = ({ todos, deleteTodo, toggleTodo }) => {
+  return (
+    <div className={styles.todoListContainer}>
+      {!todos.length && <h2>Todo list is empty</h2>}
+      {todos.map((todo) => {
+        return (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            toggleTodo={toggleTodo}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
