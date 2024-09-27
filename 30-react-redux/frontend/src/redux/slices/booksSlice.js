a30a6991890bece45dd399e4be6ccf7eb36d0afe
Viacheslav Sukhenko <Viacheslav.Sukhenko@syngenta.com>
2024-09-27 23:42:57 +0300

    another method using extraReducers

--- 30-react-redux/frontend/src/redux/slices/booksSlice.js
+++ 30-react-redux/frontend/src/redux/slices/booksSlice.js
@@ -36,6 +36,15 @@ const booksSlice = createSlice({
       });
     },
   },
+
+  // extraReducers: {
+  //   [fetchBook.fulfilled]: (state, action) => {
+  //     if (action.payload && action.payload.title && action.payload.author) {
+  //       state.push(createBookWithId(action.payload, 'API'));
+  //     }
+  //   }
+  // }
+
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
       if (action.payload && action.payload.title && action.payload.author) {
